cc85443f141cd96d35e0087fb761159f3bb24b85
alexzander-pages <cruz.alexzander.day@gmail.com>
2026-02-21 19:15:08 -0500

    Credit Card Form

---
+++ creditcard/javascript/creditcard.js
@@ -0,0 +1,46 @@
+    const cardForm = document.querySelector('#cardFrontGrid');
+    const creditInput = document.querySelector('#cardFrontGrid input');
+
+function displayError(msg) {
+	// display error message
+	document.querySelector('.errors').textContent = msg
+}
+
+function isCardNumberValid(number) {
+	return number === '1234123412341234'
+}
+
+function submitHandler(event) {
+	event.preventDefault();
+  let errorMsg = '';
+	displayError('');
+
+  let cardNumber = document.querySelector('#creditCardNumber');
+  const cardNum = cardNumber.value.trim();
+    
+    if (!/^\d{16}$/.test(cardNum)) {
+        errorMsg += 'Card number must be 16 digits\n';
+    } else if (!isCardNumberValid(cardNum)) {
+        errorMsg += 'Card number is not valid\n';
+    }
+    
+    const expYear = Number(document.querySelector('#year').value);   
+    const expMonth = Number(document.querySelector('#month').value);
+    const currentDate = new Date()
+
+    if (2000 + expYear < currentDate.getFullYear() || (2000 + expYear === currentDate.getFullYear() && expMonth <= (currentDate.getMonth() + 1))
+    ) {
+        errorMsg += 'Card is expired\n';
+    }
+
+    if (errorMsg !== '') {
+		displayError(errorMsg)
+		return;
+    }
+    const formContainer = document.getElementById('cardForm');
+    formContainer.innerHTML = '<h2>Thank you for your purchase.</h2>';
+}
+
+  
+document.querySelector('#cardForm').addEventListener('submit', submitHandler)
+          
